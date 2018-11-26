023ba97279d07a1b3c8d77cd8f4f9acee799da8a
Pawan Kumar3 <pawank3@chetu.com>
2018-11-26 21:11:07 +0530

    initial commit

---
+++ app/src/main/java/com/hackingbuzz/gridview1/MainActivity.java
@@ -0,0 +1,28 @@
+package com.hackingbuzz.gridview1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+       // GridView people = (GridView)findViewById(R.id.people);
+
+        ExpandableHeightGridView  myListView = (ExpandableHeightGridView) findViewById(R.id.people);
+        myListView.setExpanded(true);
+
+        CustomAdapter adapter = new CustomAdapter(getLayoutInflater());
+
+        myListView.setAdapter(adapter);
+
+
+    }
+}
